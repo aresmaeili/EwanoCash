bf03ad16d0474656ab20d6e200dc03ab9310c23a
Reza Kashkoul <rezakashkoul@gmail.com>
2021-09-29 17:26:45 +0330

    Implementing UI Elements

---
+++ EwanoCash/HomePage/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  EwanoCash
+//
+//  Created by Reza Kashkoul on 7/7/1400 AP.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

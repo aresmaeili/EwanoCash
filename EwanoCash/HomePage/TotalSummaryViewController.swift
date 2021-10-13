053f7f68873993772c505d372316ed24103e42c9
Ahmadreza <lovlyboy@gmail.com>
2021-10-13 23:21:30 +0330

    Improvements for TotalSummaryViewController.

--- EwanoCash/HomePage/TotalSummaryViewController.swift
+++ EwanoCash/HomePage/TotalSummaryViewController.swift
@@ -6,55 +6,123 @@
 //
 
 import UIKit
+import AAInfographics
 
 class TotalSummaryViewController: UIViewController {
     
-    @IBOutlet var summaryChartView: UIView!
+    @IBOutlet var chartParentView: UIView!
     @IBOutlet weak var totalSpendingAmountLabel: UILabel!
     @IBOutlet weak var totalIncomeAmountLabel: UILabel!
-    @IBOutlet weak var totalSummaryTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
-    var items = ["bill" , "buying show" , "coffee" , "taxi" , "bill" , "buying show" , "coffee" , "taxi" , "bill" , "buying show" , "coffee" , "taxi"]
+    var chartView = AAChartView()
+    var isTableAutoReloadEnabled = true
+    var items: [TransfersModel] = [] {
+        didSet {
+            if true {
+                DispatchQueue.main.async { [self] in
+                    tableView.reloadData()
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        totalSummaryTableView.delegate = self
-        totalSummaryTableView.dataSource = self
+        
         navigationItem.title = "Total Summary"
-        totalSummaryTableView.separatorStyle = .none
-        summaryChartView.layer.cornerRadius = 25
-        summaryChartView.clipsToBounds = true
-        totalSummaryTableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
-        tabBarController?.selectedIndex = 2
+        setpTableView()
+        stupChartView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loadChartData()
+        fillSummaryLabels()
+    }
+    
+    func setpTableView() {
+        tableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+    }
+    
+    func stupChartView() {
+        chartParentView.layer.cornerRadius = 25
+        chartParentView.clipsToBounds = true
+        chartView.frame = chartParentView.bounds
+        chartParentView.addSubview(chartView)
+    }
+    
+    func fillSummaryLabels() {
+        totalIncomeAmountLabel.text = items.filter({$0.isIncome}).compactMap({Int($0.amountOfTransaction) ?? 0}).reduce(0, +).description + "$"
+        totalSpendingAmountLabel.text = items.filter({!$0.isIncome}).compactMap({Int($0.amountOfTransaction) ?? 0}).reduce(0, +).description + "$"
     }
 }
 
-extension TotalSummaryViewController : UITableViewDelegate , UITableViewDataSource {
+// MARK: - Data manager
+extension TotalSummaryViewController {
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
-        cell.itemTitle.text = items[indexPath.row]
-        cell.itemPrice.text = ""
-        return cell
+    func loadChartData() {
+        isTableAutoReloadEnabled = false
+        items = getData()
+        isTableAutoReloadEnabled = true
+        let allTransactions = items.compactMap({Int($0.amountOfTransaction)})
+        let data = AAChartModel()
+            .chartType(.spline)
+            .animationType(.easeInCubic)
+            .dataLabelsEnabled(false)
+            .touchEventEnabled(false)
+            .tooltipEnabled(false)
+            .legendEnabled(false)
+            .markerSymbolStyle(.borderBlank)
+            .colorsTheme(["#0000FF"])
+            .categories(allTransactions.compactMap({$0.description}))
+            .series([
+                AASeriesElement()
+                    .name("Expences")
+                    .data(allTransactions),
+//                AASeriesElement()
+//                    .name("")
+//                    .data(allTransactions.compactMap({$0})),
+            ])
+        chartView.aa_drawChartWithChartModel(data)
     }
+    
+    func getData()-> [TransfersModel] {
+        if let data = UserDefaults.standard.value(forKey:"listOfTransactions") as? Data {
+            if let transferData = try? PropertyListDecoder().decode(Array<TransfersModel>.self, from: data) {
+                return transferData
+            }
+        }
+        return []
+    }
+}
+
+// MARK: - TableView Functions
+extension TotalSummaryViewController : UITableViewDelegate , UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
-            tableView.beginUpdates()
-            items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            tableView.endUpdates()
-            print(items)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
+        let item = items[indexPath.row]
+        cell.itemTitle.text = item.titleOfTransaction
+        cell.itemDate.text = item.dateOfTransaction.getPrettyDate()
+        cell.itemPrice.text = item.amountOfTransaction
+        if item.isIncome == true {
+            cell.itemImage.image = UIImage(named: "chevron_down")
+        } else {
+            cell.itemImage.image = UIImage(named: "chevron_up")
         }
-        return
+        return cell
     }
     
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
-    
 }
